5b80ef3730de55651c9ff3f3af1ef19cc4366c3d
Avishrant Sharma <avishrants@gmail.com>
2022-03-11 21:08:52 +0530

    Initialize app, update formatting
    Signed-off-by: Avishrant Sharma <avishrants@gmail.com>

--- etc/structure/project_name/settings.py
+++ etc/structure/project_name/settings.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
-import environ
 
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -14,7 +14,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*'] if DEBUG else []
+ALLOWED_HOSTS = ["*"] if DEBUG else []
 
 
 # Application definition
